773286db4af984c727028c62b3cdcf24c80c184a
Kristian Perez <kpere192@fiu.edu>
2024-04-07 13:24:25 -0400

    Got display working with multiple pass through of equations, but the code is ugly so will be refactoring

--- javascript.js
+++ javascript.js
@@ -1,21 +1,72 @@
 let displayText = "";
+let currOperation = [];
 
-function updateDisplay( btn )
+function updateDisplay(btn)
 {
-  displayText += btn;
+  displayText = updateOperation(btn).join(" ");
   const display = document.querySelector(".display");
   const displayBox = document.querySelector(".displayBox");
   displayBox.textContent = displayText;
 }
 
+function updateOperation (btn)
+{
+  // If input is not a number, means its an operation
+  if(isNaN(btn))
+  {
+    if (currOperation.length == 1)
+    {
+      currOperation[1] = btn;
+    }
+  }
+
+  else
+  {
+      if(currOperation.length == 0)
+      {
+        currOperation[0] = btn.toString();
+      }
+      else if (currOperation.length == 1)
+      {
+        currOperation[0] += btn.toString();
+      }
+      else
+      {
+        if (currOperation[2] === undefined)
+        {
+            currOperation[2] = btn.toString();
+        }
+        else
+        {
+            currOperation[2] += btn.toString();
+        }
+      }
+  }
+  return currOperation;
+}
+
 function clearDisplay()
 {
   displayText = "";
+  currOperation = [];
   const displayBox = document.querySelector(".displayBox");
   displayBox.textContent = displayText;
 }
 
-function operate( num1, operator, num2)
+function doOperation()
+{
+    currOperation[0] = operate(currOperation[0], currOperation[1], currOperation[2]);
+    let temp = currOperation[0];
+    currOperation = [];
+    currOperation.push(temp);
+    displayText = currOperation;
+    console.log(currOperation);
+    console.log(displayText);
+    const displayBox = document.querySelector(".displayBox");
+    displayBox.textContent = displayText;
+}
+
+function operate(num1, operator, num2)
 {
   let output = "";
   if(operator == "+")
@@ -24,9 +75,9 @@ function operate( num1, operator, num2)
   }
   else if(operator == "-")
   {
-    output = substract(num1, num2);
+    output = subtract(num1, num2);
   }
-  else if(operator == "*")
+  else if(operator == "x")
   {
     output = multiply(num1, num2);
   }
@@ -38,13 +89,12 @@ function operate( num1, operator, num2)
   {
     console.error("Operation doesnt exist");
   }
-
   return output;
 }
 
 function add(num1, num2)
 {
-  return num1 + num2;
+  return +num1 + +num2;
 }
 
 function subtract(num1, num2)
